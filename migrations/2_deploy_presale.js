47a284d0628673ec84f2cb6f1cbd366e046a8760
David <34934826+viewo@users.noreply.github.com>
2018-03-13 10:33:17 +0200

    Update 2_deploy_presale.js
    Updated migration

--- migrations/2_deploy_presale.js
+++ migrations/2_deploy_presale.js
@@ -1,10 +1,5 @@
 var PresaleViewo = artifacts.require("./PresaleViewo.sol");
 
-module.exports = function(deployer, network) {
-    if(network !== 'development'){
-        deployer.deploy(PresaleViewo).then(async ()=> {
-            let presale = await PresaleViewo.deployed();
-            console.log(presale.address);
-        })
-    }
+module.exports = function(deployer) {
+        deployer.deploy(PresaleViewo);
 };
