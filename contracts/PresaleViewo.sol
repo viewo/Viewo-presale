3c9c2f610fbd578dd4a4bf0c4a0dda0b0f35384a
David <34934826+viewo@users.noreply.github.com>
2018-03-15 13:00:56 +0200

    Update PresaleViewo.sol
    Updating from zeppelin-solidity and removing constant

--- contracts/PresaleViewo.sol
+++ contracts/PresaleViewo.sol
@@ -1,27 +1,42 @@
 pragma solidity 0.4.18;
 
-library SafeMath {
-  function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+library SafeMath {  
+   /**
+  * @dev Multiplies two numbers, throws on overflow.
+  */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
     uint256 c = a * b;
-    assert(a == 0 || c / a == b);
+    assert(c / a == b);
     return c;
   }
 
-  function div(uint256 a, uint256 b) internal constant returns (uint256) {
-    uint256 c = a / b;
+  /**
+  * @dev Integer division of two numbers, truncating the quotient.
+  */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {    
+    uint256 c = a / b;    
     return c;
   }
 
-  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+   /**
+  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+  */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     assert(b <= a);
     return a - b;
   }
 
-  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+  /**
+  * @dev Adds two numbers, throws on overflow.
+  */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
     assert(c >= a);
     return c;
-  }
+  }  
 }
 
 contract Ownable {
