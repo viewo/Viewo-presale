843553914b36e3dade6cff8daeac5b3f4526d4b8
David <34934826+viewo@users.noreply.github.com>
2018-03-15 13:54:41 +0200

    Update PresaleViewo.sol
    Updated to use view from constant

--- contracts/PresaleViewo.sol
+++ contracts/PresaleViewo.sol
@@ -106,8 +106,8 @@ contract Claimable is Ownable {
 }
 
 contract ERC20Basic {
-  uint256 public totalSupply;
-  function balanceOf(address who) public constant returns (uint256);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address who) public view returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
 }
